d08fe7f2eed9fe2af65054c087099c0a8be17190
Jesús <thejedex@gmail.com>
2018-12-02 20:57:06 +0100

    Nuevo

---
+++ ej1b.s
@@ -0,0 +1,24 @@
+	AREA	ej1,CODE,READWRITE
+
+SWI_Salir	EQU	&11
+RESULTADO	DCD	1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20	; Vector de 4 bytes cada uno
+
+	ENTRY
+	
+	MOV 	r0, #0		; Registro r0 (donde se almacena el resultado) inicializado a 0
+	MOV 	r2, #0		; Contador inicializado a 0
+	ADRL	r1, RESULTADO	; r1 se convierte en puntero al vector RESULTADO
+	B COND			; Salto incondicional a COND (simulando bucle while)
+
+BUCLE	LDR	r3,[r1]		; Cargamos primera palabra de r1 en r3
+	ADD	r0,r0,r3		; Sumamos a r0 el contenido de la posición del vector en la que estamos
+	ADD	r1,r1,#4		; Avanzamos una palabra en vector para pasar al siguiente numero
+	ADD	r2,r2,#1		; Sumamos 1 a contador
+
+COND	CMP	r2, #20		; Si contador es menor que 20, saltar a etiqueta BUCLE	
+	BLT	BUCLE
+	
+	SWI SWI_Salir
+
+
+	END
